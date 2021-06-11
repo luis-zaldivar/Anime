22eb8c20138b58e0eaa59354a53e03a7b86ce397
luis-zaldivar <lalozaldivarbarza@gmail.com>
2021-06-11 00:51:10 -0500

    creacion de las ventanas

---
+++ src/anime/Anime.java
@@ -0,0 +1,11 @@
+package anime;
+import Interfaz.*;
+public class Anime {
+    public static void main(String[] args) {
+        
+        Inicio ini=new Inicio();
+        ini.setVisible(true);
+        
+    }
+    
+}
